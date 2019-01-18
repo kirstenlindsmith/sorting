6c3afb0d41edcc28888b701aa04c16b1233ac06c
Sara Parelhoff <sara.parelhoff@gmail.com>
2019-01-18 17:06:14 -0500

    changes so far

--- bubblesort.js
+++ bubblesort.js
@@ -1,11 +1,24 @@
-function swap(){
-    
+function swap(array, index){
+   let swapNumber = array.splice(index, 1)
+   array.splice(index + 1, 0, swapNumber) 
 }
 
-function compare(){
-  
+function compare(left, right){
+  if (left < right) return true
+  return false
 }
 
 function bubbleSort(array) {
-  
+    let swapCounter = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        const currElement = array[i]
+        const nextElement = array[i + 1]
+        if (!compare(currElement, nextElement)) {
+            swap(array, i, array[i])
+            swapCounter++
+        }
+    }
+
+    return array
 }
